3819b25ed213b7632a1591a9371f955f2ed6a036
ruben <2140816@MY.IPLEIRIA.PT>
2017-03-15 20:49:50 +0000

    add BusStops in Map with Titles

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController, ToastController } from 'ionic-angular';
 
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 import * as L from 'leaflet';
 
 @Component({
@@ -10,60 +12,67 @@ import * as L from 'leaflet';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+  private stops: any;
+  public map: any;
+
+  private iconBus = L.icon({
+    iconUrl: 'assets/icon/android-bus.png',
+    iconSize: [35, 35]
+  });
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public http: Http) {
   }
 
   ngOnInit(): void {
-    var iconBus = L.icon({
-      iconUrl: 'assets/icon/android-bus.png',
-      //iconRetinaUrl: 'my-icon@2x.png',
-      iconSize: [35, 35],
-      //iconAnchor: [22, 94],
-      //popupAnchor: [-3, -76],
-      //shadowUrl: 'my-icon-shadow.png',
-      //shadowRetinaUrl: 'my-icon-shadow@2x.png',
-      //shadowSize: [68, 95],
-      //shadowAnchor: [22, 94]
-    });
+    this.initMap();
+    this.getStopsStations();
 
 
-    let map = L.map('mapid')
+    // this.map.on('locationerror', this.onLocationError);
+    // this.map.on('locationfound', this.onLocationFound);
+  }
+
+  initMap(): void {
+    this.map = L.map('mapid')
       .setView([39.7460465, -8.8059954], 14);
-    map.locate({ setView: true });
+    this.map.locate({ setView: true });
 
     L.tileLayer('https://api.mapbox.com/styles/v1/rcdd/cj0b89eqw005a2sqh9zew1bew/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
-      // attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       attribution: 'Application power by RP&RD :)',
-      // minZoom: 12.5,
-      maxZoom: 18,
-      // bounds: bounds,
-      bounceAtZoomLimits: false,
+      maxZoom: 20,
       id: 'mapbox.mapbox-traffic-v1',
       accessToken: 'sk.eyJ1IjoicmNkZCIsImEiOiJjajBiOGhzOGUwMDF3MzNteDB1MzJpMTl6In0.1fiOkskHZqGiV20G95ENaA'
-    }).addTo(map);
+    }).addTo(this.map);
+  }
 
-    function onLocationFound(e) {
-      var radius = e.accuracy / 2;
+  onLocationFound(e) {
+    var _this = this;
+    var radius = e.accuracy / 2;
+    L.marker(e.latlng).addTo(this.map)
+      .bindPopup("You are within " + radius + " meters from this point").openPopup();
+    L.circle(e.latlng, radius).addTo(_this.map);
+  }
 
-      L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-      L.circle(e.latlng, radius).addTo(map);
-    }
-
-    function onLocationError(e) {
-      //this.showToast('User denied localization. For better performance, please allow location.', 3000);
-      alert('User denied localization. For better performance, please allow your location.');
-    }
-
-    map.on('locationerror', onLocationError);
-
-    map.on('locationfound', onLocationFound);
-
-    L.marker([39.7460465, -8.8059954], { icon: iconBus }).addTo(map);
+  onLocationError(e) {
+    //this.showToast('User denied localization. For better performance, please allow location.', 3000);
+    alert('User denied localization. For better performance, please allow your location.');
   }
 
 
+  getStopsStations() {
+    return this.http.get(`http://194.210.216.191/otp/routers/default/index/stops`)
+      .map((res: Response) => res.json()).subscribe(a => {
+        this.stops = a;
+        //console.log(this.stops);
+        this.stops.forEach(stop => {
+          L.marker([stop.lat, stop.lon], { icon: this.iconBus, title: stop.name })
+            .bindPopup(stop.name)
+            .addTo(this.map).on('click', function (e) {
+              this.openPopup();
+            });
+        });
+      });
+  }
   showToast(msg: string, ms: number): void {
     let toast = this.toastCtrl.create({
       message: msg,
