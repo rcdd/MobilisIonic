e003be7e11111b99047c7e36bd42a56162b4229c
ruben <2140816@MY.IPLEIRIA.PT>
2017-05-11 22:02:02 +0100

    tabs changes

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -11,7 +11,7 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
+  //tab2Root: any = AboutPage;
   tab3Root: any = ContactPage;
 
   constructor() {
